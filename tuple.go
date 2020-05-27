8edaf49805805a977c84a4bcacf040a3e991c182
Jimmy Kiang <castlevanity@gmail.com>
2020-05-27 21:54:47 +0200

    Add tuple

--- tuple.go
+++ tuple.go
@@ -1,12 +1,34 @@
 package main
 
 //Tuple of four floating points.
+// Point when w == 1
+// vector when w == 0
 type Tuple struct {
 	x, y, z, w float64
+}
+
+//Point creates a tuple representing a point (w == 1)
+func Point(x, y, z float64) *Tuple {
+	return &Tuple{x, y, z, 1.0}
+}
+
+// Vector creates a tuple representing a vector (w == 0)
+func Vector(x, y, z float64) *Tuple {
+	return &Tuple{x, y, z, 0.0}
 }
 
 //Equals returns true if x, y, z, w from tuples t and o are within the error margin Epsilon.
 func (t *Tuple) Equals(o *Tuple) bool {
 
 	return floatEqual(t.x, o.x) && floatEqual(t.y, o.y) && floatEqual(t.z, o.z) && floatEqual(t.w, o.w)
+}
+
+//Add tuples.
+func (t *Tuple) Add(o *Tuple) *Tuple {
+	return &Tuple{
+		x: t.x + o.x,
+		y: t.y + o.y,
+		z: t.z + o.z,
+		w: t.w + o.w,
+	}
 }
--- tuple_test.go
+++ tuple_test.go
@@ -17,3 +17,24 @@ func TestTupleEqual(t *testing.T) {
 		t.Errorf("TupleEqual: %v should not equal %v", a, b)
 	}
 }
+
+func TestAddTuple(t *testing.T) {
+	a := Point(3.0, -2.0, 5.0)
+	b := Vector(-2.0, 3.0, 1.0)
+
+	result := a.Add(b)
+	expected := Point(1.0, 1.0, 6.0)
+	pass := result.Equals(expected)
+	if !pass {
+		t.Errorf("AddTuple: result %v should equal %v", result, expected)
+	}
+	a = Vector(3.0, -2.0, 5.0)
+	b = Vector(-2.0, 3.0, 1.0)
+
+	result = a.Add(b)
+	expected = Vector(1.0, 1.0, 6.0)
+	pass = result.Equals(expected)
+	if !pass {
+		t.Errorf("AddTuple: result %v should equal %v", result, expected)
+	}
+}
