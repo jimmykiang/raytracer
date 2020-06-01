65b068bc84e3e00984aa2d29d1c132afae687e0d
Jimmy Kiang <castlevanity@gmail.com>
2020-06-01 13:35:19 +0200

    color: substract and multiplyByScalar

--- color.go
+++ color.go
@@ -15,6 +15,16 @@ func (c *Color) Add(o *Color) *Color {
 	return NewColor(c.r+o.r, c.g+o.g, c.b+o.b)
 }
 
+// Subtract operation for 2 colors.
+func (c *Color) Subtract(o *Color) *Color {
+	return NewColor(c.r-o.r, c.g-o.g, c.b-o.b)
+}
+
+// MultiplyByScalar operation by a scalar.
+func (c *Color) MultiplyByScalar(scalar float64) *Color {
+	return NewColor(c.r*scalar, c.g*scalar, c.b*scalar)
+}
+
 // Equals returns true if the r, g, b from tuples t and o are within the error margin Epsilon.
 func (c *Color) Equals(o *Color) bool {
 	return floatEqual(c.r, o.r) && floatEqual(c.g, o.g) && floatEqual(c.b, o.b)
--- color_test.go
+++ color_test.go
@@ -14,3 +14,29 @@ func TestAddColor(t *testing.T) {
 		t.Errorf("AddColor: result %v should equal %v", result, expected)
 	}
 }
+
+func TestSubColor(t *testing.T) {
+	a := NewColor(0.9, 0.6, 0.75)
+	b := NewColor(0.7, 0.1, 0.25)
+	result := a.Subtract(b)
+	expected := NewColor(0.2, 0.5, 0.5)
+
+	pass := result.Equals(expected)
+
+	if !pass {
+		t.Errorf("SubColor: result %v should equal %v", result, expected)
+	}
+}
+
+func TestMultiplybyScalarColor(t *testing.T) {
+	a := NewColor(0.2, 0.3, 0.4)
+	scalar := 2.0
+	result := a.MultiplyByScalar(scalar)
+	expected := NewColor(0.4, 0.6, 0.8)
+
+	pass := result.Equals(expected)
+
+	if !pass {
+		t.Errorf("MulScalarColor: result %v should equal %v", result, expected)
+	}
+}
