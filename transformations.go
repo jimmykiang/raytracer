211ebca320eab995b2f8ab19e957816d46f1caa6
Jimmy Kiang <castlevanity@gmail.com>
2020-06-27 13:02:40 +0200

    scaling

--- transformations.go
+++ transformations.go
@@ -10,3 +10,12 @@ func Translation(x, y, z float64) Matrix {
 
 	return matrix
 }
+
+// Scaling returns a scale matrix
+func Scaling(x, y, z float64) Matrix {
+	matrix := NewIdentityMatrix()
+	matrix.Set(0, 0, x)
+	matrix.Set(1, 1, y)
+	matrix.Set(2, 2, z)
+	return matrix
+}
--- transformations_test.go
+++ transformations_test.go
@@ -32,3 +32,41 @@ func TestTranslation(t *testing.T) {
 	}
 
 }
+
+func TestScaling(t *testing.T) {
+	transform := Scaling(2, 3, 4)
+	p := Point(-4, 6, 8)
+
+	result := transform.MultiplyMatrixByTuple(p)
+	expected := Point(-8, 18, 32)
+
+	if !result.Equals(expected) {
+		t.Errorf("Scaling:Point expected %v to equal %v", result, expected)
+	}
+
+	v := Vector(-4, 6, 8)
+
+	result = transform.MultiplyMatrixByTuple(v)
+	expected = Vector(-8, 18, 32)
+
+	if !result.Equals(expected) {
+		t.Errorf("Scaling:Vector expected %v to equal %v", result, expected)
+	}
+
+	inv := transform.Inverse()
+	result = inv.MultiplyMatrixByTuple(v)
+	expected = Vector(-2, 2, 2)
+
+	if !result.Equals(expected) {
+		t.Errorf("Scaling:Vector (inverse) expected %v to equal %v", result, expected)
+	}
+
+	transform = Scaling(-1, 1, 1)
+	p = Point(2, 3, 4)
+	result = transform.MultiplyMatrixByTuple(p)
+	expected = Point(-2, 3, 4)
+
+	if !result.Equals(expected) {
+		t.Errorf("Scaling:Point (reflection) expected %v to equal %v", result, expected)
+	}
+}
