ff9c814ea01d48d909ba73b5d6d878fd2b3fa862
jimmykiang <castlevanity@gmail.com>
2020-08-09 12:06:10 +0200

    OBJ File with Polygon Data.

--- wavefrontObjParser_test.go
+++ wavefrontObjParser_test.go
@@ -57,21 +57,65 @@ f 1 3 4
 	t2 := group.children[1].(*Triangle)
 
 	if !(t1.p1.Equals(parser.vertices[1])) {
-		t.Errorf("Ignoring unrecognized lines, got: %v and expected to be %v", t1.p1, parser.vertices[1])
+		t.Errorf("Parsing triangle faces, got: %v and expected to be %v", t1.p1, parser.vertices[1])
 	}
 	if !(t1.p2.Equals(parser.vertices[2])) {
-		t.Errorf("Ignoring unrecognized lines, got: %v and expected to be %v", t1.p2, parser.vertices[2])
+		t.Errorf("Parsing triangle faces, got: %v and expected to be %v", t1.p2, parser.vertices[2])
 	}
 	if !(t1.p3.Equals(parser.vertices[3])) {
-		t.Errorf("Ignoring unrecognized lines, got: %v and expected to be %v", t1.p3, parser.vertices[3])
+		t.Errorf("Parsing triangle faces, got: %v and expected to be %v", t1.p3, parser.vertices[3])
 	}
 	if !(t2.p1.Equals(parser.vertices[1])) {
-		t.Errorf("Ignoring unrecognized lines, got: %v and expected to be %v", t2.p1, parser.vertices[1])
+		t.Errorf("Parsing triangle faces, got: %v and expected to be %v", t2.p1, parser.vertices[1])
 	}
 	if !(t2.p2.Equals(parser.vertices[3])) {
-		t.Errorf("Ignoring unrecognized lines, got: %v and expected to be %v", t2.p2, parser.vertices[3])
+		t.Errorf("Parsing triangle faces, got: %v and expected to be %v", t2.p2, parser.vertices[3])
 	}
 	if !(t2.p3.Equals(parser.vertices[4])) {
-		t.Errorf("Ignoring unrecognized lines, got: %v and expected to be %v", t2.p3, parser.vertices[4])
+		t.Errorf("Parsing triangle faces, got: %v and expected to be %v", t2.p3, parser.vertices[4])
+	}
+}
+
+func TestTriangulatePolygon(t *testing.T) {
+	// Triangulating polygons.
+	data := `
+v -1 1 0
+v -1 0 0
+v 1 0 0
+v 1 1 0
+v 0 2 0
+f 1 2 3 4 5`
+	parser := parseObjData(data)
+	group := parser.defaultGroup()
+	t1 := group.children[0].(*Triangle)
+	t2 := group.children[1].(*Triangle)
+	t3 := group.children[2].(*Triangle)
+
+	if !(t1.p1.Equals(parser.vertices[1])) {
+		t.Errorf("Triangulating polygons, got: %v and expected to be %v", t1.p1, parser.vertices[1])
+	}
+	if !(t1.p2.Equals(parser.vertices[2])) {
+		t.Errorf("Triangulating polygons, got: %v and expected to be %v", t1.p2, parser.vertices[2])
+	}
+	if !(t1.p3.Equals(parser.vertices[3])) {
+		t.Errorf("Triangulating polygons, got: %v and expected to be %v", t1.p3, parser.vertices[3])
+	}
+	if !(t2.p1.Equals(parser.vertices[1])) {
+		t.Errorf("Triangulating polygons, got: %v and expected to be %v", t2.p1, parser.vertices[1])
+	}
+	if !(t2.p2.Equals(parser.vertices[3])) {
+		t.Errorf("Triangulating polygons, got: %v and expected to be %v", t2.p2, parser.vertices[3])
+	}
+	if !(t2.p3.Equals(parser.vertices[4])) {
+		t.Errorf("Triangulating polygons, got: %v and expected to be %v", t2.p3, parser.vertices[4])
+	}
+	if !(t3.p1.Equals(parser.vertices[1])) {
+		t.Errorf("Triangulating polygons, got: %v and expected to be %v", t3.p1, parser.vertices[1])
+	}
+	if !(t3.p2.Equals(parser.vertices[4])) {
+		t.Errorf("Triangulating polygons, got: %v and expected to be %v", t3.p2, parser.vertices[4])
+	}
+	if !(t3.p3.Equals(parser.vertices[5])) {
+		t.Errorf("Triangulating polygons, got: %v and expected to be %v", t2.p3, parser.vertices[5])
 	}
 }
