a0e1573abd8e25a4109df18f3a276709cf6b6656
jimmykiang <castlevanity@gmail.com>
2020-08-18 17:09:39 +0200

    Splitting an x,y,z wide box

--- bvh_test.go
+++ bvh_test.go
@@ -24,3 +24,75 @@ func TestSplitPerfectCube(t *testing.T) {
 		t.Errorf("Splitting a perfect cube: got %v, expected: %v", right.max, expectedRightMaxPoint)
 	}
 }
+
+func TestSplitXWideBoundingBox(t *testing.T) {
+	// Splitting an x-wide box.
+
+	box := NewBoundingBoxFloat(-1, -2, -3, 9, 5.5, 3)
+	left, right := SplitBounds(box)
+
+	expectedLeftMinPoint := Point(-1, -2, -3)
+	if !(expectedLeftMinPoint.Equals(left.min)) {
+		t.Errorf("Splitting an x-wide box: got %v, expected: %v", left.min, expectedLeftMinPoint)
+	}
+	expectedLeftMaxPoint := Point(4, 5.5, 3)
+	if !(expectedLeftMaxPoint.Equals(left.max)) {
+		t.Errorf("Splitting an x-wide box: got %v, expected: %v", left.max, expectedLeftMaxPoint)
+	}
+	expectedRightMinPoint := Point(4, -2, -3)
+	if !(expectedRightMinPoint.Equals(right.min)) {
+		t.Errorf("Splitting an x-wide box: got %v, expected: %v", right.min, expectedRightMinPoint)
+	}
+	expectedRightMaxPoint := Point(9, 5.5, 3)
+	if !(expectedRightMaxPoint.Equals(right.max)) {
+		t.Errorf("Splitting an x-wide box: got %v, expected: %v", right.max, expectedRightMaxPoint)
+	}
+}
+
+func TestSplitYWideBoundingBox(t *testing.T) {
+	// Splitting an x-wide box.
+
+	box := NewBoundingBoxFloat(-1, -2, -3, 5, 8, 3)
+	left, right := SplitBounds(box)
+
+	expectedLeftMinPoint := Point(-1, -2, -3)
+	if !(expectedLeftMinPoint.Equals(left.min)) {
+		t.Errorf("Splitting an x-wide box: got %v, expected: %v", left.min, expectedLeftMinPoint)
+	}
+	expectedLeftMaxPoint := Point(5, 3, 3)
+	if !(expectedLeftMaxPoint.Equals(left.max)) {
+		t.Errorf("Splitting an x-wide box: got %v, expected: %v", left.max, expectedLeftMaxPoint)
+	}
+	expectedRightMinPoint := Point(-1, 3, -3)
+	if !(expectedRightMinPoint.Equals(right.min)) {
+		t.Errorf("Splitting an x-wide box: got %v, expected: %v", right.min, expectedRightMinPoint)
+	}
+	expectedRightMaxPoint := Point(5, 8, 3)
+	if !(expectedRightMaxPoint.Equals(right.max)) {
+		t.Errorf("Splitting an x-wide box: got %v, expected: %v", right.max, expectedRightMaxPoint)
+	}
+}
+
+func TestSplitZWideBoundingBox(t *testing.T) {
+	// Splitting an x-wide box.
+
+	box := NewBoundingBoxFloat(-1, -2, -3, 5, 3, 7)
+	left, right := SplitBounds(box)
+
+	expectedLeftMinPoint := Point(-1, -2, -3)
+	if !(expectedLeftMinPoint.Equals(left.min)) {
+		t.Errorf("Splitting an x-wide box: got %v, expected: %v", left.min, expectedLeftMinPoint)
+	}
+	expectedLeftMaxPoint := Point(5, 3, 2)
+	if !(expectedLeftMaxPoint.Equals(left.max)) {
+		t.Errorf("Splitting an x-wide box: got %v, expected: %v", left.max, expectedLeftMaxPoint)
+	}
+	expectedRightMinPoint := Point(-1, -2, 2)
+	if !(expectedRightMinPoint.Equals(right.min)) {
+		t.Errorf("Splitting an x-wide box: got %v, expected: %v", right.min, expectedRightMinPoint)
+	}
+	expectedRightMaxPoint := Point(5, 3, 7)
+	if !(expectedRightMaxPoint.Equals(right.max)) {
+		t.Errorf("Splitting an x-wide box: got %v, expected: %v", right.max, expectedRightMaxPoint)
+	}
+}
