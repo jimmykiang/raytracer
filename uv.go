b5ebd35e6b407946fcb902e7308b2da786810d2b
jimmykiang <castlevanity@gmail.com>
2020-08-22 17:51:58 +0200

    Using a spherical mapping on a 3D point.

--- uv.go
+++ uv.go
@@ -28,7 +28,37 @@ func uvPatternAt(uvCheckers *UVCheckers, u, v float64) *Color {
 
 	if (u2+v2)%2 == 0 {
 		return uvCheckers.colorA
-	} else {
-		return uvCheckers.colorB
 	}
+	return uvCheckers.colorB
+}
+
+func sphericalMap(point *Tuple) (u, v float64) {
+
+	// Compute the azimuthal angle (-π < theta <= π).
+	// Angle increases clockwise as viewed from above, which is opposite of what we want, but we'll fix it later.
+	theta := math.Atan2(point.x, point.z)
+
+	// vec is the vector pointing from the sphere's origin (the world origin)
+	// to the point, which will also happen to be exactly equal to the sphere's radius.
+	vec := Vector(point.x, point.y, point.z)
+	radius := vec.Magnitude()
+
+	// Compute the polar angle
+	// 0 <= phi <= π
+	phi := math.Acos(point.y / radius)
+
+	// -0.5 < raw_u <= 0.5
+	rawU := theta / (2 * PI)
+
+	// 0 <= u < 1
+	// here's also where we fix the direction of u. Subtract it from 1,
+	// so that it increases counterclockwise as viewed from above.
+	u = 1 - (rawU + 0.5)
+
+	// We want v to be 0 at the south pole of the sphere,
+	// and 1 at the north pole, so we have to "flip it over"
+	// by subtracting it from 1.
+	v = 1 - phi/PI
+
+	return
 }
--- uv_test.go
+++ uv_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func Test2DCheckerPattern(t *testing.T) {
 	// Checker pattern in 2D.
@@ -26,6 +29,40 @@ func Test2DCheckerPattern(t *testing.T) {
 		if !(uvPatternAt(checkers, val.u, val.v) == val.expectedColor) {
 			t.Errorf("Checker pattern in 2D, got: %v and expected to be %v",
 				uvPatternAt(checkers, val.u, val.v), val.expectedColor)
+		}
+	}
+}
+
+func TestSphericalMappingOn3DPoint(t *testing.T) {
+	// Using a spherical mapping on a 3D point.
+
+	type testStruct struct {
+		expectedU float64
+		expectedV float64
+		point     *Tuple
+	}
+
+	expectedTest := []testStruct{
+		{point: Point(0, 0, -1), expectedU: 0.0, expectedV: 0.5},
+		{point: Point(1, 0, 0), expectedU: 0.25, expectedV: 0.5},
+		{point: Point(0, 0, 1), expectedU: 0.5, expectedV: 0.5},
+		{point: Point(-1, 0, 0), expectedU: 0.75, expectedV: 0.5},
+		{point: Point(0, 1, 0), expectedU: 0.5, expectedV: 1},
+		{point: Point(0, -1, 0), expectedU: 0.5, expectedV: 0},
+		{point: Point((math.Sqrt(2) / 2), (math.Sqrt(2) / 2), 0), expectedU: 0.25, expectedV: 0.75},
+	}
+
+	for _, val := range expectedTest {
+
+		u, v := sphericalMap(val.point)
+
+		if !floatEqual(val.expectedU, u) {
+			t.Errorf("Using a spherical mapping on a 3D point, got: %v and expected to be %v",
+				u, val.expectedU)
+		}
+		if !floatEqual(val.expectedV, v) {
+			t.Errorf("Using a spherical mapping on a 3D point, got: %v and expected to be %v",
+				v, val.expectedV)
 		}
 	}
 }
